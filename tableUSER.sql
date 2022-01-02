503071b68410e1f7c93dc776ee865ae644a61651
ADIRZ100 <81367573+ADIRZ100@users.noreply.github.com>
2022-01-02 19:08:54 +0200

    Add files via upload

---
+++ tableUSER.sql
@@ -0,0 +1,17 @@
+USE [SIMON]
+GO
+
+/****** Object:  Table [dbo].[User1]    Script Date: 02/01/2022 19:08:25 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[User1](
+	[firstname] [varchar](20) NULL,
+	[lastname] [varchar](20) NULL,
+	[Birthday] [varchar](50) NULL
+) ON [PRIMARY]
+GO
+
